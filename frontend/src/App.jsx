1cba5569fbd8ff1774c755626cab67f61f85a88b
Ajith J <ajmarar005@gmail.com>
2026-04-01 02:47:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     
     const fetchBudget = async () => {
       try {
-        const res = await axios.get('http://localhost:5001/api/budget/today');
+        const res = await axios.get('https://aquawatch-1.onrender.com');
         setBudgetData(res.data);
       } catch (err) {
         console.error('Error fetching budget:', err);
